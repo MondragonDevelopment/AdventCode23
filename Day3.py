e758f035f1f0caa6cfe3f34f48f1645f5f96e9dc
Mondragon <fimv705@gmail.com>
2023-12-05 17:23:41 -0600

    Day 3 full
    Solved a bug where it ignored squared number gears

--- Day3.py
+++ Day3.py
@@ -22,8 +22,8 @@ def isPartNumber(matrix):
         rownums.append(currnums)
         rowsyms.append(currsyms)
         partSum += checkAdjacency(lastrowsyms, lastrownums, currsyms, currnums)
-    print('nums in matrix:', rownums)
-    print('symbols in matrix:', rowsyms)
+    #print('nums in matrix:', rownums)
+    #print('symbols in matrix:', rowsyms)
     return partSum
 
 
@@ -59,11 +59,98 @@ def checkAdjacency(ls, ln,  cs, cn):
     return adjacent
 
 
+def gearRatio(matrix):
+    rownums = []
+    rowsyms = []
+    currnums = []
+    ratioSum = 0
+    for row in range(len(matrix)):
+        lastrownums = currnums
+        i = len(matrix[row]) - 1
+        currnums = []
+        currsyms = []
+        while i>0:
+            #print(i)
+            if matrix[row][i].isdigit():
+                cont = numberIs(matrix[row][:(i + 1)])
+                i = cont[0] + 1
+                currnums = [[cont[1], max(0, i-1), min(i + cont[2], len(matrix[row]) - 1)]] + currnums
+            elif matrix[row][i] != '.':
+                currsyms = [[matrix[row][i], i]] + currsyms
+            i -= 1
+        rownums.append(currnums)
+        rowsyms.append(currsyms)
+        if row < len(matrix) - 1:
+            nextnums = nextRow(matrix[row+1])
+        else:
+            nextnums = []
+        ratioSum += isGear(lastrownums, currsyms, currnums, nextnums)
+    #print('nums in matrix:', rownums)
+    #print('symbols in matrix:', rowsyms)
+    return ratioSum
+
+
+def nextRow(nr):
+    nextnums = []
+    i = len(nr) - 1
+    while i > 0:
+        # print(i)
+        if nr[i].isdigit():
+            cont = numberIs(nr[:(i + 1)])
+            i = cont[0] + 1
+            nextnums = [[cont[1], max(0, i - 1), min(i + cont[2], len(nr) - 1)]] + nextnums
+        i -= 1
+    return nextnums
+
+
+def isGear(ln, cs, cn, nn):
+    gearratio = 0
+    if len(cs) != 0:
+        #print(ln, cs, cn, nn)
+        for i in range(len(cs)):
+            if cs[i][0] == '*':
+                adjacent = 0
+                stack = []
+                #print(cs[i], i)
+                while adjacent <= 2:
+                    for num in ln:
+                        #if num[0] in stack: continue
+                        if num[1] <= cs[i][1] <= num[2]:
+                            stack.append(num[0])
+                            #print(cn[i][0])
+                            adjacent += 1
+                            #print(stack)
+                    for num in cn:
+                        #if num[0] in stack: continue
+                        if num[1] <= cs[i][1] <= num[2]:
+                            stack.append(num[0])
+                            #print(cn[i][0])
+                            adjacent += 1
+                            #print(stack)
+                    for num in nn:
+                        #if num[0] in stack: continue
+                        if num[1] <= cs[i][1] <= num[2]:
+                            stack.append(num[0])
+                            #print(cn[i][0])
+                            adjacent += 1
+                            #print(stack)
+                    break
+                #print(adjacent)
+                if adjacent == 2:
+                    gearratio += stack[0]*stack[1]
+                    # print(stack, stack[0]*stack[1])
+                #if adjacent >= 2:
+                    #print(i, gearratio)
+    return gearratio
+
+
+
 with open('data/data3') as data:
     matrix = []
     for line in data:
         matrix.append(line[:-1])
     print(isPartNumber(matrix))
+    print(gearRatio(matrix))
 
 
 print()
